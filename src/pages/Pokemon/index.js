0987e09107851e7a5b87485a77ddcffa88906a84
Truong Cong Hieu <truongconghieuptit@gmail.com>
2023-09-24 14:28:34 +0700

    add loading feature and handle error

--- src/pages/Pokemon/index.js
+++ src/pages/Pokemon/index.js
@@ -1,20 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Pokemon = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleFetchPokemon = async () => {
-    const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
-    setPokemons(response.data.results);
+  const handleFetchPokemon = () => {
+    setIsLoading(true);
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => {
+        setPokemons(response.data.results);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        setIsLoading(false);
+      });
   };
+
+  useEffect(() => {
+    handleFetchPokemon();
+  }, []);
 
   return (
     <div className="pokemon">
-      <button onClick={handleFetchPokemon}>Fetch Pokemon</button>
+      {isLoading && <h4>Loading...</h4>}
       <ul>
         {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>{pokemon.name}</li>
+          <li key={pokemon.name}>{pokemon.name}</li>
         ))}
       </ul>
     </div>
