e8cb8b998bba6450a9891a86b00f6442d30070f4
Truong Cong Hieu <truongconghieuptit@gmail.com>
2023-10-05 20:05:13 +0700

    add deploy feature

--- src/components/NameCard/index.js
+++ src/components/NameCard/index.js
@@ -4,10 +4,7 @@ const NameCard = ({ name, age, gender = "?" }) => {
   const classNameCard = `card ${gender !== "?" ? gender.toLowerCase() : ""}`;
 
   return (
-    <div
-      className={classNameCard}
-      onClick={() => console.log("You click", name)}
-    >
+    <div className={classNameCard} onClick={() => {}}>
       <div className="name">Name: {name}</div>
       <div className="age">Age: {age}</div>
       <div className="gender">Gender: {gender}</div>
